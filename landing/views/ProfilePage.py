27daf5760ff37653f0deb4e43d7a140744d9832a
sometimesago1 <awescom@yandex.ru>
2025-03-08 21:31:09 +0600

    TailwindCSS upgrated to version 4.0

---
+++ landing/views/ProfilePage.py
@@ -0,0 +1,4 @@
+from django.views.generic import DetailView
+
+class ProfilePage(DetailView):
+    pass
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,15 @@ module.exports = {
         'neutral-150': '#F1F1F1',
         'neutral-750': '#2D2D32',
         'neutral-850': '#222225'
-      }
+      },
+      borderRadius: {
+        '4xl': '32px',
+        '5xl': '36px',
+        '6xl': '48px',
+        '7xl': '56px',
+        '8xl': '64px',
+        '9xl': '72px',
+      },
     },
   },
   plugins: [],
